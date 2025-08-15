cb24ab1753b58c65a7b8a38a570f2e4d034566d8
ivangarzab <ivangb6@gmail.com>
2025-08-15 13:57:56 -0700

    Add iOS sample app

---
+++ sample-ios/barK-sample/barK-sample/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  barK-sample
+//
+//  Created by Ivan Garza on 8/15/25.
+//
+
+import SwiftUI
+//import shared
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}

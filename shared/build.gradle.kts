36a209a09496c1d93c0f048512bb8f2e7b367f01
Iván Garza Bermea <ivangb6@gmail.com>
2026-04-04 01:34:43 -0700

    docs: Impl Dokka (#32)
    * docs: Update README.md after bad merge
    
    * docs(dokka): Prep docs for dokka implementation
    
    * feat: Implement dokka into the :shared module
    
    * cicd(dokka): Update docs.yml CI
    
    * cicd: Update docs.yml's Dokka impl

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.kover)
+    alias(libs.plugins.dokka)
     `maven-publish`
     signing
 }
